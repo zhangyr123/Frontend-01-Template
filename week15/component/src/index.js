198f41dd2048d911f96289e3642cd1fd407a11b2
zhangyr <2195175822@qq.com>
2020-07-23 09:40:49 +0800

    week15

---
+++ week15/component/src/index.js
@@ -0,0 +1,8 @@
+require('./foo.js')
+
+function create() {
+    console.log(arguments)
+}
+let component = <div id="a" />
+
+console.log('13214')
---
+++ week15/component/webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require("webpack");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader'
+        }]
+    },
+    mode: "development",
+    optimization: {
+        minimize: false
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
